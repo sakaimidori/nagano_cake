e99907cab43171e648f1635134bf08d5d0598be7
sakaimidori <104720565+sakaimidori@users.noreply.github.com>
2022-09-13 17:15:41 +0000

    fix:製作ステータスを全て「製作完了」にすると、注文ステータスが「発送準備中」に更新されるよう修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -4,20 +4,15 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     #管理者／注文詳細画面の制作ステータス更新
-
-
-    #@order.update(order_params)
-    #@order_items = @order.order_items
     @order_item = OrderItem.find(params[:id])
     @order_item.update(order_item_params)
     if @order_item.product_status == "making"
       @order_item.order.update(order_status: 2)
     end
-    @order = Order.find(params[:id])
-    #@order_items = @order.order_items
-    if @order_items.product_status == "finish_making"
+     @order = @order_item.order
+     @order_items = @order.order_items
+    if @order.order_items.where(product_status:"finish_making").count == @order.order_items.count
       @order_item.order.update(order_status: 3)
-
     end
     redirect_to admin_order_path(@order_item.order)
   end
